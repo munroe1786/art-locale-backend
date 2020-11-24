1faaa5c2960df076f8bb9bf1ef1c8d8f51744885
munroe1786 <era3munroe@gmail.com>
2020-11-24 13:55:10 -0600

    set up api v1 namespaced controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :museums
-  resources :artists
+  
+  namespace :api do
+    namespace :v1 do
+      resources :museums
+      resources :artists
+    end 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#fetch(url) - normally - ('http://localhost:3000')
+
+#want to incorporate name spacing with api v1 included
