7d91a5c587e11b391fa408b51eb92e134f09aa42
munroe1786 <era3munroe@gmail.com>
2020-11-24 15:09:02 -0600

    stub out controllers

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::ArtistsController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :style)
+    end
+
+
+
+
+
+
 end
--- app/controllers/api/v1/museums_controller.rb
+++ app/controllers/api/v1/museums_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::MuseumsController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def museum_params
+
+    end
+
+
+
+
 end
