9bedcf4ffb85fb833174a5cbf45e4e8e368ae518
munroe1786 <era3munroe@gmail.com>
2020-11-24 16:43:20 -0600

    building out museum controller

--- app/controllers/api/v1/museums_controller.rb
+++ app/controllers/api/v1/museums_controller.rb
@@ -1,28 +1,34 @@
 class Api::V1::MuseumsController < ApplicationController
 
-    def index
+    before_action :set_artist
 
+    def index
+       #need index of museums associated with a certain artist
     end
 
     def create
-
+        
     end
 
     def show
-
+        
     end
 
     def destroy
-
+        
     end
 
     private
 
-    def museum_params
-
+    def set_artist
+        #need to create an artist variable that we can use over and over
+        @artist = Artist.find(params[:artist_id])
+        #params is coming from the url with the artist ID in it
     end
 
-
-
+    #ex request to /api/v1/artists/1/museums
+    def museum_params
+        params.require(:museum).permit(:artist_id, :name, :location, :description)
+    end
 
 end
