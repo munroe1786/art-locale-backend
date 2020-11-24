adeb638c1b1760500e025787933c8c4c6a3b48c5
munroe1786 <era3munroe@gmail.com>
2020-11-24 15:28:13 -0600

    add artist index method

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ArtistsController < ApplicationController
 
     def index
-
+        @artists = Artist.all
+        render json: @artists 
+        #saying - render these accounts as json
+        #render json: @artists - @artists is the response in our fetch requests
+        #will be sent back to our front end after a fetch request is made into here
     end
 
     def create
