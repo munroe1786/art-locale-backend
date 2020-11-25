44fa417863e2f13605f892b617967c0cc550a571
munroe1786 <era3munroe@gmail.com>
2020-11-25 08:01:19 -0600

    add artist attributes and relationship to serializer / add museum attributes to serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,3 +1,4 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :style
+  has_many :museums
 end
--- app/serializers/museum_serializer.rb
+++ app/serializers/museum_serializer.rb
@@ -1,3 +1,7 @@
 class MuseumSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :location, :description, :artist_id
 end
+
+#routes are nested, so don't have to have artist_id in the
+#serializer.  I'm leaving it in case I want to redesign the 
+#routes
